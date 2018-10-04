19c5d8af72e8a149dc3394edb450e9e03a5ac187
harshbones <davranjar@hotmail.com>
2018-10-04 13:17:48 +0200

    ajout journalists.rb

---
+++ journalists.rb
@@ -0,0 +1,115 @@
+# méthode afin de simplifier l'affichage sur terminal en ajoutant une pause après chaque ajout de données
+def continue
+  puts
+  puts "Appuyez sur ENTREE pour continuer."
+  gets
+end
+
+# méthode contenant l'array
+def liste
+  @journalistes = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+# affiche le nombre d'éléments dans l'array journalistes
+def nombreliste
+  puts
+  puts "Le nombre de journalistes de la liste est de #{@journalistes.length}"
+  continue
+end
+
+# détermine une variable journalisteschiffres à partir de journalistes
+# .grep permet de ne chercher que les éléments contenant les caractères entre /[ ]/ , ici de 0 à 9
+# affiche la variable journalisteschiffres
+def chiffreliste
+  journalisteschiffres = @journalistes.grep /[0-9]/
+  puts "Le nombre de journalistes dont le compte contient un chiffre est de #{journalisteschiffres.length}"
+  continue
+end
+
+# détermine une variable nombreaude en compteur
+# vérifie poue chaque élément de journalistes si il contient le terme "aude" sans faire attention à la casse
+# si la vérification est vraie, ajoute 1 au compteur puis affiche la variable du compteur
+def aude
+  nombreaude = 0
+  @journalistes.each do |na|
+    if na.index (/aude/i)
+      nombreaude += 1
+    end
+                    end
+  puts "Le nombre de journalistes dont le compte contient Aude est de #{nombreaude}"
+  continue
+end
+
+# détermine une variable nombremajfirst à partir de journalistes
+# .select permet de sélectionner les éléments de l'array correspondant au décrit entre {}
+# Ici, on demande à ce que le caractère 1 de l'élément ( donc celui juste après le @ ) soit en capitales et ne soit pas un underscore
+# affiche par la suite le nombre d'éléments présents dans nombremajfirst
+def majliste
+  nombremajfirst = @journalistes.select { |dude|
+                                        dude[1] == dude[1].capitalize && dude[1] != "_"
+                                      }
+  puts "Le nombre de journalistes dont le compte commence par une majuscule est de #{nombremajfirst.length}"
+  continue
+end
+
+# détermine une variable journalistesmajall à partir de journalistes
+# .grep permet de ne chercher que les éléments contenant les caractères entre /[ ]/ , ici de A à Z ( majuscules )
+# affiche la variable journalistesmajall
+def majalllistes
+  journalistesmajall = @journalistes.grep /[A-Z]/
+  puts "Le nombre de journalistes dont le compte contient une majuscule est de #{journalistesmajall.length}"
+  continue
+end
+
+# détermine une variable journalistes_ qui sert de compteur
+# pour chaque élément de l'array journaliste, déterminés en une variable j_, effectue l'action suivante :
+# remplit une variable machin avec .chars qui permet de décomposer les caractères d'une variable en éléments d'un Array
+# détermine une variable numberunderscore à partir de la variable machin
+# sélectionne chacun des éléments de la variable machin et vérifie si il s'agit d'un underscore
+# si vrai, le rajoute au compteur de journalistes_
+# affiche la variable journalistes_
+def underscore
+  journalistes_ = 0
+  @journalistes.each do |j_|
+    machin = j_.chars
+    numberunderscore = machin.select { |underscore|
+                                        underscore == "_"
+                                      }
+    journalistes_ += numberunderscore.length
+                    end
+  puts "Le nombre de journalistes dont le compte contient un underscore est de #{journalistes_}"
+  continue
+end
+
+# affiche un message en caps lock
+# .sort_by permet de classer alphabétiquement les caractères
+# .downcase permet de ne pas faire attention à la casse en considérant les éléments en minuscules
+def alphabetliste
+  puts "DEBUT DE LA LISTE DES JOURNALISTES PAR ORDRE ALPHABETIQUE"
+  10.times do print "-" end
+  print @journalistes.sort_by { |sorter| sorter.downcase }
+  puts
+  10. times do print "-" end
+  puts
+  puts "FIN DE LA LISTE DES JOURNALISTES PAR ORDRE ALPHABETIQUE"
+  continue
+end
+
+liste
+nombreliste
+chiffreliste
+aude
+majliste
+majalllistes
+underscore
+alphabetliste
+
+=begin
+  Recette pour un bon punch cubain
+    Prenez  une mesure de rhum ambré,
+            une mesure de jus d'orange,
+            une mesure de jus d'ananas,
+    Toppez avec de la limonade,
+    Ajoutez du sirop de grenadine,
+    Mélangez et servez frais
+=end
